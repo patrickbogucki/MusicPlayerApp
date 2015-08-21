71cd05a7f2de8ecab3006a5e25fc184ea7ec4fd2
squirrelxseeker <patrickbogucki@gmail.com>
2015-08-21 03:22:28 -0700

    Initialized project. Added necessary gulp and npm files for starting up and automation such as browser reload.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('styles', function(){
+  gulp.src(['dev/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(gulp.dest('dev/css/'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('deploy', function() {
+	// minify, uglify, concatenate, move files
+	});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("dev/**/*.scss", ['styles']);
+  gulp.watch("*.html", ['bs-reload']);
+  gulp.watch("dev/**/*.js", ['bs-reload']);
+});
