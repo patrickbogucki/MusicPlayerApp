cfa454c748d42e09e2a4222bbce08346cd8727b4
squirrelxseeker <patrickbogucki@gmail.com>
2015-08-21 03:41:37 -0700

    Added routing for main and search page including changing to use ng-view in index.html.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('MusicPlayerApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'MainController',
+			templateUrl: 'dev/views/main.html'
+		})
+		.when('/search', {
+			controller: 'SearchController',
+			templateUrl: 'dev/views/search.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
