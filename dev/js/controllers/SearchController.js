77a9bfe8ed68eb33496f4551b07ddf89b4184453
squirrelxseeker <patrickbogucki@gmail.com>
2015-08-28 02:17:58 -0700

    Removed depracated success and error and used then to get response.

--- dev/js/controllers/SearchController.js
+++ dev/js/controllers/SearchController.js
@@ -1,22 +1,21 @@
 app.controller('SearchController', ['$scope', '$http', function($scope, $http) {
 	$scope.artist = '';
 	$scope.getArtists = function() {
-		return $http.get('https://api.spotify.com/v1/search?q=%22' + $scope.artist + '%22&type=artist')
-         .success(function(data) {
-           $scope.artists = data.artists.items;
-           $scope.errorMsg = '';
-         })
-         .error(function(err) {
-         	$scope.artists = '';
-           $scope.errorMsg = err.error.message;
-         });
+		$http.get('https://api.spotify.com/v1/search?q=%22' + $scope.artist + '%22&type=artist')
+		.then(function(response) {
+			$scope.artists = response.data.artists.items;
+			$scope.errorMsg = '';
+		}, function(response) {
+			$scope.artists = '';
+			$scope.errorMsg = response.error.message;
+		});
 	};
 
 
 	$scope.remove = function($event) {
 		// console.log($index);
 		// $scope.artists.splice($index, 1);
-		 console.log(event.target.className);
+		console.log(event.target.className);
 
 	};
 
